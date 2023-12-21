ac06f3b6591d447e9b6936c349a6b67320327bd0
TheIthorian <ben.shortland1020@gmail.com>
2023-12-21 18:33:00 +0000

    feat: add start scripts

---
+++ scripts/restart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run Maven build
+./mvnw clean package -DskipTests
+
+# Copy JAR to docker directory
+cp target/spring-project-0.0.1-SNAPSHOT.jar src/docker
+
+# Change to docker directory
+cd src/docker
+
+# Stop and remove existing containers
+docker-compose down
+
+# Remove existing Docker image
+docker rmi spring-project:latest
+
+# # Build and run Docker containers
+# docker-compose up
---
+++ scripts/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd src/docker
+
+sudo docker-compose up
